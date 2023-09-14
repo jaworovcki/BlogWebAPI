3354a763d4a9fc2bb287b94ed9d327575fc3fcc0
Andrii <jaworovcki@gmail.com>
2023-09-14 19:37:26 +0300

    Create the domain models

---
+++ API/CodePulse.API/Models/Domain/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+	public class Category
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+        public string UrlHandle { get; set; } = string.Empty;
+    }
+
+}
