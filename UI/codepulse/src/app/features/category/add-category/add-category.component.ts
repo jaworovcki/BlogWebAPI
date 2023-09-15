88683cb835dc950f69894dd1d81249a614137aba
Andrii <jaworovcki@gmail.com>
2023-09-15 22:00:53 +0300

    Add service to post category

--- UI/codepulse/src/app/features/category/add-category/add-category.component.ts
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-add-category',
@@ -11,7 +12,7 @@ export class AddCategoryComponent {
 
   model : AddCategoryRequest;
 
-  constructor() {
+  constructor(private categoryService : CategoryService) {
     this.model = {
       name : '',
       urlHandle : ''
@@ -19,7 +20,12 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit() {
-    console.log(this.model);
+    this.categoryService.addCategory(this.model)
+      .subscribe({
+        next: (response) => {
+          console.log("Success");
+        }
+      });
   }
 
 }
--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,10 @@ import { Injectable } from '@angular/core';
 
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  addCategory(model : AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(`https://localhost:7000/api/categories`, model);
+
+  }
 }
